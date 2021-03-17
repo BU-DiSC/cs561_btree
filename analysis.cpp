cbb20dcfb53fbbdb8145f14dcf9e23a33cad3040
Aneesh Raman <aneeshr@bu.edu>
2021-03-17 17:02:40 -0400

    added info for basic tree implementation

--- analysis.cpp
+++ analysis.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 int main()
 {
+    // init a tree that takes integers for both key and value
+    // the first argument is the name of the block manager for the cache (can be anything)
+    // second argument is the directory where the block manager will operate (if changing this, make sure to update makefile)
+    // 3rd argument is the size of every block in bytes. Suggest to keep it as is since 4096B = 4KB = 1 page
+    // 4th argument is the number of blocks to store in memory. Right now, it is at 10000 blocks = 10000*4096 bytes = 40.96 MB
+    // potentially, the only argument to change would be the last one (4th arg) to increase the memory capacity
     BeTree<int,int> tree("manager", "./tree_dat", 4096, 10000);
 
     int n = 100000;
